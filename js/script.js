7f5290e1cabca25909a5267be74473c7261e97f8
Jin-hw <70885954+Jin-hw@users.noreply.github.com>
2021-04-14 23:54:22 +0900

    add scrollFunc

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
+let progressBar;
+
 
 window.onload = function() {
+    progressBar = document.getElementsByClassName("progressBar")[0];
 
     window.addEventListener('resize', stageResize, false);
     window.addEventListener('mousemove', mouseMove, false);
     window.addEventListener('scroll', scrollFunc, false);
 
-    stageResize();
+    //stageResize();
     loop();
 }
 
 function scrollFunc(e) {
     var scrollTop = document.documentElement.scrollTop;
 
+    let per = Math.round(scrollTop / (document.body.scrollHeight - window.innerHeight) * 100);
+    console.log(progressBar.style);
+
+    progressBar.style.width = `${per}%`;
    
 }
 
